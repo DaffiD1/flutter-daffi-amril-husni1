7e95651bf4113567314390808ddd19d66c7d0dfb
DaffiD1 <daffid1.tujuh@gmail.com>
2023-03-31 22:44:09 +0700

    mp2

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,82 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: MyWidget(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
-class MyWidget extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController controller1 = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Calculator")),
-      body: Center(
-        child: Container(
-          width: 600,
-          child: Text(
-            'Mini Project 1.',
-            maxLines: 4,
-            overflow: TextOverflow.ellipsis,
-            style: Theme.of(context).textTheme.headline4,
-          ),
-        ),
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text('Calculator'),
       ),
-    );
+      body: Column(
+        children: [
+          Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Result : 0',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold))),
+          SizedBox(
+            height: 30,
+          ),
+          Padding(
+              padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+              child: TextField(
+                controller: controller1,
+                decoration: InputDecoration(
+                    labelText: 'Enter first number',
+                    filled: true,
+                    fillColor: Colors.white),
+              )),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+              padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+              child: TextField(
+                controller: controller2,
+                decoration: InputDecoration(
+                    labelText: 'Enter second number',
+                    filled: true,
+                    fillColor: Colors.white),
+              )),
+          SizedBox(
+            height: 20,
+          ),
+          Align(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  ElevatedButton(onPressed: () {}, child: Text('Add')),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text('Substract')),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text('Multipy')),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text('Divide'))
+                ],
+              ))
+        ],
+      ),
+    ));
   }
 }
